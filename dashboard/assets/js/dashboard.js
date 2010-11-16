cf49e1b9dc7802c5e8704c4881da9b36589d8776
Marshall Sorenson <marshall.sorenson@gmail.com>
2010-11-16 14:17:33 -0500

    optimized javascript a bit

--- dashboard/assets/js/dashboard.js
+++ dashboard/assets/js/dashboard.js
@@ -1,21 +1,16 @@
-    jQuery(document).ready(function(){
-		jQuery('.rm_options').slideUp();
-		
-		jQuery('.rm_section h3').click(function(){		
-			if(jQuery(this).parent().next('.rm_options').css('display')=='none')
-				{	jQuery(this).removeClass('inactive');
-					jQuery(this).addClass('active');
-					jQuery(this).children('img').removeClass('inactive');
-					jQuery(this).children('img').addClass('active');
-					
-				}
-			else
-				{	jQuery(this).removeClass('active');
-					jQuery(this).addClass('inactive');		
-					jQuery(this).children('img').removeClass('active');			
-					jQuery(this).children('img').addClass('inactive');
-				}
-				
-			jQuery(this).parent().next('.rm_options').slideToggle('slow');	
-		});
+jQuery(document).ready(function(){
+
+	/**
+	 * Option section click event
+	 */
+	jQuery('.rm_section h3').click(function() {
+		// is the element currently displayed?
+		var displayed = (jQuery(this).parent().next('.rm_options').css('display') == 'none');
+		// toggle active class
+		jQuery(this).toggleClass('active', displayed);
+		jQuery(this).children('img').toggleClass('active', displayed);
+		// toggle slide in/out
+		jQuery(this).parent().next('.rm_options').slideToggle('slow');	
+	});
+	
 });
