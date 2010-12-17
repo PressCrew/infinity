cc04d50c085c1ee26f4f409124f3d9454bb236ae
Marshall Sorenson <marshall.sorenson@gmail.com>
2010-12-17 13:47:54 -0500

    move dashboard menu item out of buddypress menu

--- dashboard/menu.php
+++ dashboard/menu.php
@@ -16,29 +16,9 @@
  */
 function infinity_dashboard_setup_menu()
 {
-	// PIE menu
-	add_submenu_page(
-		'bp-general-settings',
-		__( 'PIE', INFINITY_TEXT_DOMAIN),
-		sprintf('<img src="%s" class="infinity-menu-icon" />', infinity_dashboard_image('pie_16x16.png') ) . __( 'PIE', INFINITY_TEXT_DOMAIN ),
-		'manage_options',
-		'infinity-pie-panel',
-		'infinity_dashboard_pie_screen' );
-
-	// Sweet control panel
-	add_submenu_page(
-		'bp-general-settings',
-		__( 'Sweet', INFINITY_TEXT_DOMAIN),
-		sprintf('<img src="%s" class="infinity-menu-icon indent" />', infinity_dashboard_image('sweet_16x16.png') ) . __( 'Sweet API', INFINITY_TEXT_DOMAIN ),
-		'manage_options',
-		'infinity-sweet-panel',
-		'infinity_dashboard_sweet_screen' );
-
-	// Infinity control panel
-	add_submenu_page(
-		'bp-general-settings',
+	add_object_page(
 		__( 'Infinity', INFINITY_TEXT_DOMAIN),
-		sprintf('<img src="%s" class="infinity-menu-icon indent" />', infinity_dashboard_image('infinity_16x16.png') ) . __( 'Infinity Theme', INFINITY_TEXT_DOMAIN ),
+		__( 'Infinity', INFINITY_TEXT_DOMAIN),
 		'manage_options',
 		'infinity-control-panel',
 		'infinity_dashboard_cpanel_screen' );
