9f0ff41b9d6536b0f187c60d5683e402299e29b7
Marshall Sorenson <marshall.sorenson@gmail.com>
2010-12-18 08:14:35 -0500

    added blank templates for all cpanel pages

--- dashboard/templates/pie.php
+++
@@ -1,7 +0,0 @@
-<div class="wrap nosubsub">
-	<div class="icon32"><img src="<?php print infinity_dashboard_image( 'pie_32x32.png' ) ?>" /></div>
-	<h2><?php _e( 'PIE', INFINITY_TEXT_DOMAIN ) ?></h2>
-	<p>
-		Talk about PIE
-	</p>
-</div>
