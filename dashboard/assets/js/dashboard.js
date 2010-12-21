7d6f0c6f46298d1a00e584e2795fdcd4bdb5a2ac
Marshall Sorenson <marshall.sorenson@gmail.com>
2010-12-21 13:58:58 -0500

    first round of moving cpanel to jui

--- dashboard/assets/js/dashboard.js
+++ dashboard/assets/js/dashboard.js
@@ -1,16 +1,26 @@
-jQuery(document).ready(function(){
+(function($){
+	$(document).ready(function() {
 
-	/**
-	 * Option section click event
-	 */
-	jQuery('.rm_section h3').click(function() {
-		// is the element currently displayed?
-		var displayed = (jQuery(this).parent().next('.rm_options').css('display') == 'none');
-		// toggle active class
-		jQuery(this).toggleClass('active', displayed);
-		jQuery(this).children('img').toggleClass('active', displayed);
-		// toggle slide in/out
-		jQuery(this).parent().next('.rm_options').slideToggle('slow');	
+		$('a#infinity-cpanel-toolbar-start').button({icons: {primary: "ui-icon-power"}});
+		$('a#infinity-cpanel-toolbar-widgets').button({icons: {primary: "ui-icon-gear"}});
+		$('a#infinity-cpanel-toolbar-shortcodes').button({icons: {primary: "ui-icon-copy"}});
+		$('a#infinity-cpanel-toolbar-options').button({icons: {primary: "ui-icon-wrench"}});
+		$('a#infinity-cpanel-toolbar-docs').button({icons: {primary: "ui-icon-document"}});
+		$('a#infinity-cpanel-toolbar-about').button({icons: {primary: "ui-icon-person"}});
+		$('a#infinity-cpanel-toolbar-thanks').button({icons: {primary: "ui-icon-heart"}});
+
+		/**
+		 * Option section click event
+		 */
+		$('.rm_section h3').click(function() {
+			// is the element currently displayed?
+			var displayed = ($(this).parent().next('.rm_options').css('display') == 'none');
+			// toggle active class
+			$(this).toggleClass('active', displayed);
+			$(this).children('img').toggleClass('active', displayed);
+			// toggle slide in/out
+			$(this).parent().next('.rm_options').slideToggle('slow');
+		});
+
 	});
-	
-});
+})(jQuery);
--- dashboard/templates/docs.php
+++ dashboard/templates/docs.php
@@ -1,3 +1,1 @@
-<div class="infinity-cpanel-docs">
-	<?php infinity_dashboard_doc_publish( 'docs', infinity_dashboard_route_param( 1 ) ); ?>
-</div>
+<?php infinity_dashboard_doc_publish( 'docs', infinity_dashboard_route_param( 1 ) ); ?>
