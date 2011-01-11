91ae03f99e492c4d6558970cc0f1744ff0d3bc45
Marshall Sorenson <marshall.sorenson@gmail.com>
2011-01-11 18:35:18 -0500

    more ajax tweaks

--- dashboard/loader.php
+++ dashboard/loader.php
@@ -74,7 +74,10 @@ function infinity_dashboard_cpanel_setup()
 			INFINITY_ADMIN_PAGE,
 			'InfinityDashboardL10n',
 			array(
-				'ajax_url' => get_site_url( 1, 'wp-admin/admin-ajax.php' )
+				'ajax_url' =>
+					is_admin() ?
+						admin_url( 'admin-ajax.php' ) :
+						get_site_url( 1, 'wp-admin/admin-ajax.php' )
 			)
 		);
 
