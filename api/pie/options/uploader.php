2af3a37c2d8dc349ea0e2e10264e739b44c2e38d
Marshall <marshall@ubu.(none)>
2011-01-14 11:42:00 -0500

    replaced thickbox with jui dialogue

--- api/pie/options/uploader.php
+++ api/pie/options/uploader.php
@@ -103,15 +103,18 @@ class Pie_Easy_Options_Uploader
 	public function render( Pie_Easy_Options_Option $option, Pie_Easy_Options_Option_Renderer $renderer )
 	{
 		$edit_url = sprintf( 'media.php?attachment_id=%d&action=edit', $option->get() );
-		$attach_url = $option->get_image_url('full'); ?>
+		list( $attach_url, $attach_width, $attach_height ) = $option->get_image_src('full'); ?>
 		<div class="pie-easy-options-fu">
 			<fieldset class="pie-easy-options-fu-img">
 				<legend><?php _e( 'Current Image' ) ?></legend>
 				<p><img src="<?php print esc_attr( $attach_url ) ?>" alt="" /></p>
-				<div>
-					<a class="thickbox" href="<?php print esc_attr( $attach_url ) ?>">Zoom</a>
+				<div class="pie-easy-options-fu-ibar">
+					<a>Zoom</a>
 					<a>Edit</a>
 					<a>Trash</a>
+				</div>
+				<div class="pie-easy-options-fu-zoom" title="Full Size Image">
+					<img src="<?php print esc_attr( $attach_url ) ?>"  height="<?php print $attach_height ?>" width="<?php print $attach_width ?>" alt="">
 				</div>
 			</fieldset>
 			<fieldset class="pie-easy-options-fu-stat">
