0d5c77b4ba5584f02383e418daea7c1e1288f75d
Marshall <marshall@ubu.(none)>
2011-04-06 16:35:09 -0400

    new cpanel theme, merged bowe designs with core

--- dashboard/templates/cpanel.php
+++ dashboard/templates/cpanel.php
@@ -1,6 +1,4 @@
 <div class="wrap nosubsub">
-	<div class="icon32"><img src="<?php print infinity_dashboard_image( 'icon_32.png' ) ?>" /></div>
-	<h2><?php _e( 'Infinity Theme', INFINITY_TEXT_DOMAIN ) ?></h2>
 	<?php
 		$actions = infinity_dashboard_cpanel_actions();
 		$current_action = infinity_dashboard_cpanel_action();
@@ -13,7 +11,6 @@
 			<?php endforeach; ?>
 			<a id="infinity-cpanel-toolbar-refresh" title="<?php _e( 'Refresh current tab', INFINITY_TEXT_DOMAIN ) ?>"><?php _e( 'Refresh', INFINITY_TEXT_DOMAIN ) ?></a>
 		</div>
-		<hr class="ui-state-default">
 		<ul id="infinity-cpanel-tabs">
 		</ul>
 	</div>
