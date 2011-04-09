87a72228006d03d595b990c409be8632d497ec07
Marshall <marshall@ubu.(none)>
2011-04-09 13:58:51 -0400

    cleaned up wp admin menu tweaks

--- dashboard/menu.php
+++ dashboard/menu.php
@@ -25,7 +25,31 @@ function infinity_dashboard_menu_setup()
 		INFINITY_ADMIN_URL . '/assets/images/icon_16.png',
 		-837 );
 
+	add_action( 'admin_print_styles', 'infinity_dashboard_menu_styler', 99999 );
 	add_action( 'admin_print_scripts', 'infinity_dashboard_menu_shifter', 99999 );
+}
+
+/**
+ * Make admin menu pretty
+ */
+function infinity_dashboard_menu_selector()
+{
+	return 'toplevel_page_' . INFINITY_ADMIN_PAGE;
+}
+
+/**
+ * Make admin menu pretty
+ */
+function infinity_dashboard_menu_styler()
+{
+	// print it ?>
+<style type="text/css">
+#adminmenu li#<?php print infinity_dashboard_menu_selector() ?> a.<?php print infinity_dashboard_menu_selector() ?> {
+	color: #285D92; font-family: Helvetica,Arial,sans-serif; font-weight: bold; text-shadow: -1px -1px #eeeeee, 2px 1px #c2c2c2; letter-spacing: 0.16em;
+	border-color: #dddddd; background: #cccccc url(<?php print INFINITY_ADMIN_URL ?>/assets/images/ui_pattern_bg.png) repeat 0px -3px;
+	box-shadow: 1px 1px #eee, 2px 2px 1px #a5a5a5; -moz-box-shadow: 1px 1px #eee, 2px 2px 1px #a5a5a5; -webkit-box-shadow: 1px 1px #eee, 2px 2px 1px #a5a5a5;
+}
+</style><?php
 }
 
 /**
@@ -35,21 +59,10 @@ function infinity_dashboard_menu_shifter()
 {
 	// print it ?>
 <script type="text/javascript">
-	jQuery(document).ready(function() {
-		var m = jQuery('li#toplevel_page_infinity-theme')
-				.addClass('menu-top-first menu-top-last')
-				.insertBefore('li#menu-posts');
-		m.children('a').first().css({
-			'color': '#285D92',
-			'background': '#cccccc url(<?php print INFINITY_ADMIN_URL ?>/assets/images/ui_pattern_bg.png) repeat 0px -3px',
-			'font-family': 'Helvetica,Arial,sans-serif',
-			'font-weight': 'bold',
-			'text-shadow': '-1px -1px #eeeeee, 2px 1px #c2c2c2',
-			'letter-spacing': '0.16em',
-			'border-color': '#dddddd'
-		});
-		jQuery('li.wp-menu-separator').first().clone().insertAfter(m);
-	});
+jQuery(document).ready(function() {
+	jQuery('li.wp-menu-separator').first().clone().insertAfter(
+		jQuery('li#<?php print infinity_dashboard_menu_selector() ?>').addClass('menu-top-first menu-top-last').insertBefore('li#menu-posts'));
+});
 </script><?php
 }
 
