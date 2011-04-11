ed3712764eb8a57dc92dcc64cc778f925a9d9500
Marshall <marshall@ubu.(none)>
2011-04-11 13:16:06 -0400

    moved docs up one directory for simplicity

--- dashboard/templates/cpanel/docs.php
+++ dashboard/templates/cpanel/docs.php
@@ -1,3 +1,3 @@
 <div class="infinity-docs">
-	<?php infinity_dashboard_doc_publish( 'docs', infinity_dashboard_route_param( 1 ) ); ?>
+	<?php infinity_dashboard_doc_publish( infinity_dashboard_route_param( 1 ) ); ?>
 </div>
