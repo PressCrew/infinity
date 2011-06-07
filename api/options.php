ff2d588818c1a4fb531a9c44c990219a79ccb873
Marshall <marshall@ubu.(none)>
2011-06-07 13:37:15 -0400

    removed stray variables

--- api/options.php
+++ api/options.php
@@ -269,11 +269,11 @@ function infinity_options_init_screen()
 {
 	// init ajax OR screen reqs (not both)
 	if ( defined( 'DOING_AJAX') ) {
-		Infinity_Options_Policy::instance()->registry($theme)->init_ajax();
+		Infinity_Options_Policy::instance()->registry()->init_ajax();
 		do_action( 'infinity_options_init_ajax' );
 	} else {
 		Pie_Easy_Enqueue::instance()->init_screen();
-		Infinity_Options_Policy::instance()->registry($theme)->init_screen();
+		Infinity_Options_Policy::instance()->registry()->init_screen();
 		do_action( 'infinity_options_init_screen' );
 	}
 }
