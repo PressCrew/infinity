3ac4df77c9f2738c076b64ff671559c7a95129d4
Marshall <marshall@ubu.(none)>
2011-07-13 15:14:14 -0400

    moved component initializers up the stack

--- base/pie/lib/features/registry.php
+++ base/pie/lib/features/registry.php
@@ -21,61 +21,7 @@ Pie_Easy_Loader::load( 'base/registry', 'features/factory' );
  */
 abstract class Pie_Easy_Features_Registry extends Pie_Easy_Registry
 {
-	/**
-	 * Init ajax requirements
-	 */
-	public function init_ajax()
-	{
-		// call parent
-		parent::init_ajax();
-
-		// init ajax for each registered feature
-		foreach ( $this->get_all() as $feature ) {
-			$feature->init_ajax();
-		}
-	}
-
-	/**
-	 * Init screen dependencies for all applicable features to be rendered
-	 */
-	public function init_screen()
-	{
-		// call parent
-		parent::init_screen();
-
-		// init screen for each registered feature
-		foreach ( $this->get_all() as $feature ) {
-			$feature->init_screen();
-		}
-	}
-
-	/**
-	 * Enqueue required styles
-	 */
-	public function init_styles()
-	{
-		// call parent
-		parent::init_styles();
-
-		// init styles for each registered feature
-		foreach ( $this->get_all() as $feature ) {
-			$feature->init_styles();
-		}
-	}
-
-	/**
-	 * Enqueue required scripts
-	 */
-	public function init_scripts()
-	{
-		// call parent
-		parent::init_scripts();
-
-		// init scripts for each registered feature
-		foreach ( $this->get_all() as $feature ) {
-			$feature->init_scripts();
-		}
-	}
+	// nothing custom yet
 }
 
 ?>
