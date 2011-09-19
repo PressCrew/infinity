e8ba32b1e57ea573a37795aa11ec1e331a3d5187
Marshall <marshall@ubu.(none)>
2011-09-19 14:43:42 -0400

    missed a couple of changes req by rendering refactor

--- base/pie/libext/features/header-logo/template.php
+++ base/pie/libext/features/header-logo/template.php
@@ -17,6 +17,6 @@
 ?>
 <<?php print $heading_tag; ?> id="site-logo">
 	<a href="<?php echo home_url( '/'  ); ?>" title="<?php bloginfo('name'); ?> - <?php bloginfo('description'); ?>">
-		<img src="<?php print $this->image_url(); ?>">
+		<img src="<?php print $this->component()->image_url(); ?>">
 	</a>
 </<?php print $heading_tag; ?>>
