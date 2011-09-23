b3cfc4834367b81938388cee581d10bb77d20747
Marshall <marshall@ubu.(none)>
2011-09-23 10:38:50 -0400

    stripped cpanel ui down to wireframe only

--- base/pie/assets/js/cpanel.js
+++ base/pie/assets/js/cpanel.js
@@ -100,7 +100,7 @@ function pieEasyCpanel(options)
 	// init cpanel tabs
 	cpanel_t.tabs({
 		tabTemplate: "<li><a href='#{href}'>#{label}</a><span class='ui-icon ui-icon-close'></span></li>",
-		panelTemplate: '<div class="pie-easy-ui-cpanel-tab"></div>',
+		panelTemplate: '<div class="pie-easy-ui-cpanel-tab ui-corner-bottom"></div>',
 		add: function(event, ui) {
 			// update tab target
 			$(ui.tab).attr('target', $(ui.tab).prop('hash').substring(1));
@@ -301,7 +301,7 @@ function pieEasyCpanel(options)
 			tp_ot = panel.offset().top;
 			tp_hc = cp_hc - (tp_ot - cp_ot);
 			cpanel.css({'height': cp_hc});
-			panel.css({'height': tp_hc - 40});
+			panel.css({'height': tp_hc - 36});
 		} else {
 			// kill heights
 			cpanel.height('auto');
