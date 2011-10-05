3104ce5b612a32466f67765cce3dc4f645e4d0a1
Marshall Sorenson <marshall.sorenson@gmail.com>
2011-10-05 12:25:08 -0400

    added export variables functionality

--- base/pie/lib/ui/script.php
+++ base/pie/lib/ui/script.php
@@ -57,6 +57,23 @@ class Pie_Easy_Script extends Pie_Easy_Asset
 	}
 
 	/**
+	 * Create and return a new logic object
+	 *
+	 * @param string $code valid javascript code
+	 * @return Pie_Easy_Script_Logic
+	 */
+	public function logic( $code = null )
+	{
+		// new logic object
+		$logic = new Pie_Easy_Script_Logic( $code );
+		
+		// add it to logic stack
+		$this->logic->push( $logic );
+
+		return $logic;
+	}
+
+	/**
 	 * Begin new logic string
 	 */
 	public function begin_logic()
@@ -71,17 +88,8 @@ class Pie_Easy_Script extends Pie_Easy_Asset
 	 */
 	public function end_logic()
 	{
-		// grab code from buffer
-		$code = ob_get_clean();
-
-		// new rule object
-		$logic = new Pie_Easy_Script_Logic( $code );
-
-		// add it to logic stack
-		$this->logic->push( $logic );
-
-		// return it for adding vars
-		return $logic;
+		// add contents of output buffer to new logic object and return it
+		return $this->logic( ob_get_clean() );
 	}
 
 	/**
@@ -120,7 +128,7 @@ class Pie_Easy_Script extends Pie_Easy_Asset
  *
  * @property boolean $alias
  * @property boolean $ready
- * @property string $function
+ * @property boolean|string $function
  * @package PIE
  * @subpackage ui
  */
@@ -217,7 +225,7 @@ class Pie_Easy_Script_Logic extends Pie_Easy_Base
 			case 'ready':
 				return $this->ready = (boolean) $value;
 			case 'function':
-				return $this->function = trim($value);
+				return $this->function = is_bool( $value ) ? $value : trim( $value );
 			default:
 				return parent::__set( $name, $value );
 		}
@@ -256,41 +264,89 @@ class Pie_Easy_Script_Logic extends Pie_Easy_Base
 	 *
 	 * @param string $name
 	 * @param mixed $value
+	 * @param boolean $nulls
 	 */
-	public function add_variable( $name, $value )
+	public function add_variable( $name, $value, $nulls = false )
 	{
-		$this->variables->add( $name, $value );
+		if ( !is_null( $value ) || $nulls ) {
+			$this->variables->add( $name, $value );
+		}
 	}
 
 	/**
 	 * Add a variable (shorthand)
 	 *
 	 * @param string $name
-	 * @param mixed $value
+	 * @param boolean $nulls
 	 */
-	public function av( $name, $value )
+	public function av( $name, $value, $nulls = false )
 	{
 		return $this->add_variable( $name, $value );
 	}
 
 	/**
-	 * Generate javascript code for this logic
+	 * Export all variables
 	 *
-	 * @param array $variables
-	 * @return string
+	 * @param boolen $object Set to true to return as an object
 	 */
-	public function export( $variables = null )
+	public function export_variables( $object = false )
 	{
-		// variables passed in?
-		if ( is_array( $variables ) ) {
-			// merge over existing vars?
-			if ( is_array( $this->variables ) ) {
-				$variables = array_merge( $this->variables->to_array(), $variables );
+		// skip if no variables
+		if ( !$this->variables->count() ) {
+			return null;
+		}
+		
+		// formatted variable statements
+		$vars = array();
+		
+		// add each variable
+		foreach ( $this->variables->to_array() as $name => $value ) {
+			// format the var
+			switch ( true ) {
+				case is_numeric( $value ):
+					$vars[$name] = $value;
+					break;
+				case is_bool( $value ):
+					$vars[$name] = ( $value ) ? 'true' : 'false';
+					break;
+				case is_null( $value ):
+					$vars[$name] = 'null';
+					break;
+				case is_string( $value ):
+					$vars[$name] = sprintf( "'%s'", $value );
+					break;
+				case is_array( $value ):
+					$vars[$name] = sprintf( "['%s']", implode( "','", $value ) );
+					break;
 			}
-		} else {
-			$variables = $this->variables->to_array();
 		}
 
+		// assignment operator
+		$operator = ($object) ? ':' : '=';
+
+		// formatted var expressions
+		$vars_f = array();
+
+		// loop all vars and format them
+		foreach( $vars as $var_name => $var_val ) {
+			array_push( $vars_f, $var_name . $operator . $var_val );
+		}
+
+		// return complete expression
+		if ( $object ) {
+			return sprintf( '{%s}', implode( ',', $vars_f ) );
+		} else {
+			return sprintf( 'var %s;', implode( ',', $vars_f ) );
+		}
+	}
+
+	/**
+	 * Generate javascript code for this logic
+	 *
+	 * @return string
+	 */
+	public function export()
+	{
 		// begin logic generation
 		$code = sprintf( '/*+++ begin logic: %s */', $this->token ) . PHP_EOL;
 
@@ -307,31 +363,16 @@ class Pie_Easy_Script_Logic extends Pie_Easy_Base
 
 		// wrap with function?
 		if ( $this->function ) {
-			$code .= sprintf( 'function %s(){', $this->function ) . PHP_EOL;
+			// anonymous function?
+			if ( $this->function === true ) {
+				$code .= sprintf( 'jQuery(function(){', $this->function ) . PHP_EOL;
+			} else {
+				$code .= sprintf( 'function %s(){', $this->function ) . PHP_EOL;
+			}
 		}
 
-		// add each variable
-		foreach ( $variables as $name => $value ) {
-			// format the var
-			switch ( true ) {
-				case is_numeric( $value ):
-					break;
-				case is_bool( $value ):
-					$value = ( $value ) ? 'true' : 'false';
-					break;
-				case is_null( $value ):
-					$value = 'null';
-					break;
-				case is_string( $value ):
-					$value = sprintf( "'%s'", $value );
-					break;
-				case is_array( $value ):
-					$value = sprintf( "['%s']", implode( "','", $value ) );
-					break;
-			}
-			// append to code
-			$code .= sprintf( "\tvar %s = %s;", $name, $value ) . PHP_EOL;
-		}
+		// add variables
+		$code .= $this->export_variables();
 
 		// add logic
 		if ( $this->logic ) {
@@ -340,7 +381,12 @@ class Pie_Easy_Script_Logic extends Pie_Easy_Base
 
 		// close function?
 		if ( $this->function ) {
-			$code .= '}' . PHP_EOL;
+			// anonymous?
+			if ( $this->function === true ) {
+				$code .= '});' . PHP_EOL;
+			} else {
+				$code .= '}' . PHP_EOL;
+			}
 		}
 
 		// close on doc ready?
