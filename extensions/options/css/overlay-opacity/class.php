d33e63801a291ac2485f5cbb9c358bfdd4f9545f
Marshall Sorenson <marshall.sorenson@gmail.com>
2012-02-17 11:37:45 -0500

    implemented infinity css overlay opacity option

---
+++ extensions/options/css/overlay-opacity/class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Infinity Theme: option extensions, CSS overlay opacity class file
+ *
+ * @author Marshall Sorenson <marshall@presscrew.com>
+ * @link http://infinity.presscrew.com/
+ * @copyright Copyright (C) 2010-2011 Marshall Sorenson
+ * @license http://www.gnu.org/licenses/gpl.html GPLv2 or later
+ * @package Infinity
+ * @subpackage extensions
+ * @since 1.0
+ */
+
+Pie_Easy_Loader::load_ext( 'options/ui/slider' );
+
+/**
+ * CSS overlay opacity
+ *
+ * @package Infinity
+ * @subpackage extensions
+ */
+class Infinity_Exts_Options_Css_Overlay_Opacity
+	extends Pie_Easy_Exts_Options_Ui_Slider
+{
+	protected function init()
+	{
+		// run parent
+		parent::init();
+
+		// init directives
+		$this->title = __( 'Overlay Opacity', infinity_text_domain );
+		$this->description = __( 'Select the overlay opacity by moving the slider', infinity_text_domain );
+		$this->default_value = 0.2;
+		$this->min = 0;
+		$this->max = 1;
+		$this->step = 0.01;
+		$this->suffix = ' level';
+		$this->style_property = 'opacity';
+	}
+}
+
+?>
