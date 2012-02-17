ad295798c4d69879480a8f6a2ba91719f3626774
Marshall Sorenson <marshall.sorenson@gmail.com>
2012-02-16 23:25:53 -0500

    Revert "Clean up and re-organize CSS and make post avatars optional"
    This reverts commit 46c1c91803ef1c00754334a30f6c7ed8a8062592.

--- base/walkers.php
+++ base/walkers.php
@@ -71,6 +71,10 @@ class Infinity_Base_Walker_Nav_Menu extends Walker_Nav_Menu
 		// close link item
 		$output .= '</li>' . PHP_EOL;
 		
+		// add menu divider to first level
+		if ( $depth < 1 ) {
+			$output .= '<li class="menu-divider"></li>' . PHP_EOL;
+		}
 	}
 }
 
