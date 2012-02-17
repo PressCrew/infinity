29ba38e20844f9dcd713b9699dfca69af609b1c2
Marshall Sorenson <marshall.sorenson@gmail.com>
2012-02-17 10:47:37 -0500

    implemented infinity css overlay image option

---
+++ extensions/options/css/overlay-image/class.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Infinity Theme: option extensions, CSS overlay image class file
+ *
+ * @author Marshall Sorenson <marshall@presscrew.com>
+ * @link http://infinity.presscrew.com/
+ * @copyright Copyright (C) 2010-2011 Marshall Sorenson
+ * @license http://www.gnu.org/licenses/gpl.html GPLv2 or later
+ * @package Infinity
+ * @subpackage extensions
+ * @since 1.0
+ */
+
+Pie_Easy_Loader::load_ext( 'options/ui/overlay-picker' );
+
+/**
+ * CSS overlay image
+ *
+ * @package Infinity
+ * @subpackage extensions
+ */
+class Infinity_Exts_Options_Css_Overlay_Image
+	extends Pie_Easy_Exts_Options_Ui_Overlay_Picker
+{
+	protected function init()
+	{
+		// run parent
+		parent::init();
+
+		// init directives
+		$this->title = __( 'Overlay Image', pie_easy_text_domain );
+		$this->description = __( 'Select a texture to use as the background overlay', pie_easy_text_domain );
+		$this->item_width = '100px';
+		$this->item_height = '100px';
+		$this->file_directory = 'assets/images/textures';
+		$this->file_extension = 'png';
+	}
+}
+
+?>
