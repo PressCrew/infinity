d07d6bc7cde96d7a0ae5f515f80a045b3b18aacb
Marshall Sorenson <marshall.sorenson@gmail.com>
2012-04-18 10:19:39 -0400

    added example files

---
+++ assets/js/example.js
@@ -0,0 +1,6 @@
+/*
+	This script is a place holder for the example
+	script configuration which is located in:
+
+	engine/config/infinity.example.ini
+*/
---
+++ engine/includes/setup.example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Infinity will automatically load a file named setup.php
+ * if it exists in this directory.
+ *
+ * You can add code directly to setup.php and also easily
+ * load additional files in this directory using the
+ * INFINITY_INC_PATH constant. For example:
+ *
+ * require_once INFINITY_INC_PATH . '/my-hooks.php';
+ */
+?>
