788424be0f352511a36e730b3994a6fc092cf1f4
Marshall Sorenson <marshall.sorenson@gmail.com>
2012-04-18 11:01:56 -0400

    renamed dev docs toc file so it wont be hidden in some IDEs

--- dashboard/templates/screens/ddocs.php
+++ dashboard/templates/screens/ddocs.php
@@ -2,6 +2,6 @@
 	<?php infinity_dashboard_devdoc_publish( infinity_screens_route_param( 1 ) ); ?>
 </div>
 <div class="infinity-docs infinity-docs-toc">
-	<?php infinity_dashboard_devdoc_publish( '.toc' ); ?>
+	<?php infinity_dashboard_devdoc_publish( '_toc' ); ?>
 </div>
 <div class="ui-helper-clearfix"></div>
