4e705c9cd20835df05f11defb92e92e528236419
hnla <hugoashmore@tiscali.co.uk>
2012-07-23 14:22:38 +0100

    Add WP language function to IE CC html tags, remove obsolete profile att from head tag to meta tag.

--- header.php
+++ header.php
@@ -12,10 +12,10 @@
  */
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7 ]>    <html lang="en" class="no-js ie6"> <![endif]-->
-<!--[if IE 7 ]>        <html lang="en" class="no-js ie7"> <![endif]-->
-<!--[if IE 8 ]>        <html lang="en" class="no-js ie8"> <![endif]-->
-<!--[if IE 9 ]>        <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if lt IE 7 ]>    <html <?php language_attributes(); ?> class="no-js ie6"> <![endif]-->
+<!--[if IE 7 ]>        <html <?php language_attributes(); ?> class="no-js ie7"> <![endif]-->
+<!--[if IE 8 ]>        <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
+<!--[if IE 9 ]>        <html <?php language_attributes(); ?> class="no-js ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
 <?php
 	do_action( 'open_meta' );
