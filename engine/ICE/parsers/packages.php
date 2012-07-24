e2245c51f561ded9bbacbf8b04f4e67b86f6e42d
Marshall Sorenson <marshall@presscrew.com>
2012-07-24 10:40:22 -0400

    added nice package lib

---
+++ engine/ICE/parsers/packages.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * ICE API: nice package class file
+ *
+ * @author Marshall Sorenson <marshall@presscrew.com>
+ * @link http://infinity.presscrew.com/
+ * @copyright Copyright (C) 2010-2011 Marshall Sorenson
+ * @license http://www.gnu.org/licenses/gpl.html GPLv2 or later
+ * @package ICE
+ * @subpackage parsers
+ * @since 1.0
+ */
+
+/**
+ * Load the markdown lib
+ */
+require_once ICE_LIB_PATH . '/nicepkg/nicepkg.php';
+
+/**
+ * This is a nice pkg wrapper
+ *
+ * @package ICE
+ * @subpackage parsers
+ */
+final class ICE_Packages extends Nice_Pkg
+{
+	// nothing special yet
+}
+
+?>
