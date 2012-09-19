fe7f528bc3b33b2748f3144bfcc7122639e289e3
Marshall Sorenson <marshall@presscrew.com>
2012-09-19 11:56:20 -0400

    has_suboption() method was using the wrong glue character

--- engine/ICE/base/component.php
+++ engine/ICE/base/component.php
@@ -929,7 +929,7 @@ abstract class ICE_Component
 	public function has_suboption( $name )
 	{
 		// build up option name
-		$option_name = sprintf( '%s-%s', $this->name, $name );
+		$option_name = $this->name . ICE_Registry::SUB_OPTION_GLUE . $name;
 
 		// get and return it
 		return $this->policy()->options()->registry()->has( $option_name );
