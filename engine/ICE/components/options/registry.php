e7c923f3974892e6a7c81d96ef2f8df4141d148d
Marshall Sorenson <marshall@presscrew.com>
2013-09-10 13:34:33 -0400

    moved global jquery ui enqueue calls to cpanel setup

--- engine/ICE/components/options/registry.php
+++ engine/ICE/components/options/registry.php
@@ -21,21 +21,6 @@ ICE_Loader::load( 'base/registry', 'components/options/factory', 'utils/ajax' );
  */
 abstract class ICE_Option_Registry extends ICE_Registry
 {
-	/**
-	 * Enqueue required scripts
-	 */
-	public function init_scripts()
-	{
-		// call parent
-		parent::init_scripts();
-
-		// jQuery UI is always needed
-		wp_enqueue_script( 'jquery-ui-accordion' );
-		wp_enqueue_script( 'jquery-ui-button' );
-		wp_enqueue_script( 'jquery-ui-dialog' );
-		wp_enqueue_script( 'jquery-ui-progressbar' );
-		wp_enqueue_script( 'jquery-ui-tabs' );
-	}
 
 	/**
 	 */
