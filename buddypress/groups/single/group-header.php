bbedacace615d81e3bb9a6ce3da02d8500ed29f3
MrMaz <marshall.sorenson@gmail.com>
2015-02-26 14:55:20 -0500

    stripped group item header avatar block from groups single group header template

--- buddypress/groups/single/group-header.php
+++ buddypress/groups/single/group-header.php
@@ -29,14 +29,6 @@ do_action( 'bp_before_group_header' );
 
 </div><!-- #item-actions -->
 
-<div id="item-header-avatar">
-	<a href="<?php bp_group_permalink(); ?>" title="<?php bp_group_name(); ?>">
-
-		<?php bp_group_avatar(); ?>
-
-	</a>
-</div><!-- #item-header-avatar -->
-
 <div id="item-header-content">
 	<span class="highlight"><?php bp_group_type(); ?></span>
 	<span class="activity"><?php printf( __( 'active %s', 'buddypress' ), bp_get_group_last_active() ); ?></span>
