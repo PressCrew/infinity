8c38bd62921c3dc87e5e7f9d370294200b588951
MrMaz <marshall.sorenson@gmail.com>
2015-02-26 16:03:39 -0500

    copied cbox single forum template verbatim

---
+++ single-forum.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Single Forum
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content-full" role="main">
+
+				<?php do_action( 'bbp_template_notices' ); ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php if ( bbp_user_can_view_forum() ) : ?>
+
+						<div id="forum-<?php bbp_forum_id(); ?>" class="bbp-forum-content">
+							<h1 class="entry-title"><?php bbp_forum_title(); ?></h1>
+							<div class="entry-content">
+
+								<?php bbp_get_template_part( 'content', 'single-forum' ); ?>
+
+							</div>
+						</div><!-- #forum-<?php bbp_forum_id(); ?> -->
+
+					<?php else : // Forum exists, user no access ?>
+
+						<?php bbp_get_template_part( 'feedback', 'no-access' ); ?>
+
+					<?php endif; ?>
+
+				<?php endwhile; ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_footer(); ?>
