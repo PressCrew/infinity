5fbaa06e10523af436145060c2f6d3fd83beb9d6
MrMaz <marshall.sorenson@gmail.com>
2015-02-26 14:56:45 -0500

    stripped item nav block from groups single home template

--- buddypress/groups/single/home.php
+++ buddypress/groups/single/home.php
@@ -10,18 +10,6 @@
 
 	</div><!-- #item-header -->
 
-	<div id="item-nav">
-		<div class="item-list-tabs no-ajax" id="object-nav" role="navigation">
-			<ul>
-
-				<?php bp_get_options_nav(); ?>
-
-				<?php do_action( 'bp_group_options_nav' ); ?>
-
-			</ul>
-		</div>
-	</div><!-- #item-nav -->
-
 	<div id="item-body">
 
 		<?php do_action( 'bp_before_group_body' );
