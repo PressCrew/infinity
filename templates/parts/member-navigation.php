e5229534dd02f5813aab07aba804e732f89a0156
MrMaz <marshall.sorenson@gmail.com>
2015-03-16 14:13:59 -0400

    trivial i18n fixes

--- templates/parts/member-navigation.php
+++ templates/parts/member-navigation.php
@@ -14,18 +14,18 @@
     <div id="profile-nav-menu">
         <?php $userLink = bp_get_loggedin_user_link();?>
         <ul>
-            <li id="edit-profile">
-            	<a class="button edit-profile-button" href="<?php echo $userLink; ?>profile/edit"><?php _e( 'Edit My Profile', 'buddypress' ); ?></a>
-            </li>
-            <li id="edit-avatar">
-            	<a class="button edit-avatar-button" href="<?php echo $userLink; ?>profile/change-avatar"><?php _e( 'Change Avatar', 'buddypress' ); ?></a>
-            </li>
-            <li id="edit-password">
-            	<a class="button edit-password-button" href="<?php echo $userLink; ?>settings"><?php _e( 'Email/Password Settings', 'cbox-theme' ); ?></a>
-            </li>
-            <li id="edit-notifications">
-            	<a class="button edit-notifications-button" href="<?php echo $userLink; ?>settings/notifications/"><?php _e( 'Notification Settings', 'cbox-theme' ); ?></a>
-            </li>
+			<li id="edit-profile">
+				<a class="button edit-profile-button" href="<?php echo $userLink; ?>profile/edit"><?php _e( 'Edit My Profile', 'infinity' ); ?></a>
+			</li>
+			<li id="edit-avatar">
+				<a class="button edit-avatar-button" href="<?php echo $userLink; ?>profile/change-avatar"><?php _e( 'Change Avatar', 'infinity' ); ?></a>
+			</li>
+			<li id="edit-password">
+				<a class="button edit-password-button" href="<?php echo $userLink; ?>settings"><?php _e( 'Email/Password Settings', 'infinity' ); ?></a>
+			</li>
+			<li id="edit-notifications">
+				<a class="button edit-notifications-button" href="<?php echo $userLink; ?>settings/notifications/"><?php _e( 'Notification Settings', 'infinity' ); ?></a>
+			</li>
         </ul>
     </div>
 	<?php endif; ?>
