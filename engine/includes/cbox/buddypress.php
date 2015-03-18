d71ff5f81311ae0e41f8b22e2568c6f6c604b88a
MrMaz <marshall.sorenson@gmail.com>
2015-03-18 14:31:43 -0400

    moved bbpress new topic button fix to core

--- engine/includes/cbox/buddypress.php
+++ engine/includes/cbox/buddypress.php
@@ -4,59 +4,6 @@
  */
 
 /**
- * Temporarily fix the "New Topic" button when using bbPress with BP.
- *
- * @todo Remove this when bbPress addresses this.
- */
-function cbox_fix_bbp_new_topic_button() {
-	// if groups isn't active, stop now!
-	if ( ! bp_is_active( 'groups' ) )
-		return;
-
-	// if bbPress 2 isn't enabled, stop now!
-	if ( ! function_exists( 'bbpress' ) )
-		return;
-
-	// remove the 'New Topic' button
-	// this is done because the 'bp_get_group_new_topic_button' filter doesn't
-	// work properly
-	remove_action( 'bp_group_header_actions', 'bp_group_new_topic_button' );
-
-	// version of bp_is_group_forum() that works with bbPress 2
-	$is_group_forum = bp_is_single_item() && bp_is_groups_component() && bp_is_current_action( 'forum' );
-
-	// If these conditions are met, this button should not be displayed
-	if ( ! is_user_logged_in() || ! $is_group_forum || bp_is_group_forum_topic()|| bp_group_is_user_banned() )
-		return false;
-
-	// create function to output new topic button
-	$new_button = create_function( '', "
-		// do not show in sidebar
-		if ( did_action( 'open_sidebar' ) )
-			return;
-
-		// render the button
-		bp_button( array(
-			'id'                => 'new_topic',
-			'component'         => 'groups',
-			'must_be_logged_in' => true,
-			'block_self'        => true,
-			'wrapper_class'     => 'group-button',
-			'link_href'         => '#new-post',    // anchor modified
-			'link_class'        => 'group-button', // removed a link_class here
-			'link_id'           => 'new-topic-button',
-			'link_text'         => __( 'New Topic', 'buddypress' ),
-			'link_title'        => __( 'New Topic', 'buddypress' ),
-		) );
-	" );
-
-	// add our customized 'New Topic' button
-	add_action( 'bp_group_header_actions', $new_button );
-
-}
-add_action( 'bp_actions', 'cbox_fix_bbp_new_topic_button' );
-
-/**
  * When running BuddyPress Docs, don't allow theme compatibility mode to kick in
  *
  * @since 1.0.5
---
+++ engine/includes/plugins.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Infinity Theme: plugins compat
+ *
+ * @author Marshall Sorenson <marshall@presscrew.com>
+ * @link http://infinity.presscrew.com/
+ * @copyright Copyright (C) 2010-2015 Marshall Sorenson
+ * @license http://www.gnu.org/licenses/gpl.html GPLv2 or later
+ * @package Infinity
+ * @subpackage base
+ * @since 1.2
+ */
+
+// is bbPress >= v2 loaded?
+if ( true === function_exists( 'bbpress' ) ) {
+	// yes, load support
+	require_once INFINITY_INC_PATH . '/plugins/bbpress.php';
+}
